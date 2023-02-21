7bb6a8ec7d0935794099834c5d7371ee256fed81
ndikumanaisaie <ndikumanaisaie@gmail.com>
2023-02-21 13:40:09 +0200

    Add like, posts and comments references to user

--- db/migrate/20230221111400_create_likes.rb
+++ db/migrate/20230221111400_create_likes.rb
@@ -1,8 +1,7 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.references :users, foreign_key: true
-      t.references :posts, foreign_key: true
+      
       t.timestamps
     end
   end
